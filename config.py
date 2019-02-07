44acbabca876fae109f0d05be3da93d5d5358751
gwy15 <gwy15thu@gmail.com>
2019-02-07 16:55:12 -0500

    remove unnecessary files

--- config.py
+++ config.py
@@ -2,4 +2,5 @@ import os
 if os.path.exists('instance') and os.path.exists('instance/config.py'):
     from instance.config import *
 
-DB_PATH = 'sqlite:///data.db'
+DB_PATH = 'Your sql db url here'
+API_KEY = 'Your Google API key here'
--- copy_table.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE download AS 
-    SELECT aid, picurl, 0 as status
-    from videos;
-CREATE INDEX aid ON download(aid);
-
