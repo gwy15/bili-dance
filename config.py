d61dba7ce9ad4fb0a58bca12c2f3a60147fa779c
gwy15 <gwy15thu@gmail.com>
2019-02-07 17:13:13 -0500

    bugfix

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
+# default settings should be overwritten by instance/config.py
+DB_PATH = 'Your sql db url here'
+API_KEY = 'Your Google API key here'
+
 import os
 if os.path.exists('instance') and os.path.exists('instance/config.py'):
     from instance.config import *
-
-DB_PATH = 'Your sql db url here'
-API_KEY = 'Your Google API key here'
